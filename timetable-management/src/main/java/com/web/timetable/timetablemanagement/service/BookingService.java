7de356f87398f897171d61b0f59d1a61dcd5122e
it21292422 <it21292422@my.sliit.lk>
2024-03-23 11:37:13 +0530

    fixed bookRoom and booService method

--- timetable-management/src/main/java/com/web/timetable/timetablemanagement/service/BookingService.java
+++ timetable-management/src/main/java/com/web/timetable/timetablemanagement/service/BookingService.java
@@ -3,6 +3,7 @@ package com.web.timetable.timetablemanagement.service;
 import com.web.timetable.timetablemanagement.model.Booking;
 import com.web.timetable.timetablemanagement.repository.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
@@ -16,19 +17,35 @@ public class BookingService {
     @Autowired
     private BookingRepository bookingRepo;
 
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
     //To check whether a room is available
     public boolean isRoomAvailable(LocalDateTime startTime, LocalDateTime endTime, String roomId){
-        List<Booking> conflictBookings = bookingRepo.findByStartTimeBetweenAndRoomId(startTime, endTime, roomId);
-        return conflictBookings.isEmpty();
-//        Query query = new Query();
-//        query.addCriteria(Criteria.where("startTime").gte(startTime).and("endTime").lte(endTime).and("roomId").is(roomId));
-//        List<Booking> bookings = mongoTemplate.find(query, Booking.class);
+        try {
+            Query query = new Query();
+            query.addCriteria(Criteria.where("startTime").gte(startTime).and("endTime").lte(endTime).and("roomId").is(roomId));
+            List<Booking> bookings = mongoTemplate.find(query, Booking.class);
+            return bookings.isEmpty();
+            //bookings.isEmpty() returns true if there are no bookings found in the database that match the specified criteria (i.e., the room is available during the specified time range),
+            // and false if there are bookings found (i.e., the room is already booked during the specified time range).
+        }catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     //To check whether a resource is available
     public boolean isResourceAvailable(LocalDateTime startTime, LocalDateTime endTime, String resourceId){
-        List<Booking> conflictBookings = bookingRepo.findByStartTimeBetweenAndResourceId(startTime, endTime, resourceId);
-        return conflictBookings.isEmpty();
+        try {
+            Query query = new Query();
+            query.addCriteria(Criteria.where("startTime").gte(startTime).and("endTime").lte(endTime).and("resourceId").is(resourceId));
+            List<Booking> bookings = mongoTemplate.find(query, Booking.class);
+            return bookings.isEmpty();
+        }catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public Booking createBooking(Booking booking){
