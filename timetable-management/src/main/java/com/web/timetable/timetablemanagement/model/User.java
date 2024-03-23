da556987dc881f53218bb9a02430ba7328805b35
it21292422 <it21292422@my.sliit.lk>
2024-03-23 11:51:27 +0530

    created user model

---
+++ timetable-management/src/main/java/com/web/timetable/timetablemanagement/model/User.java
@@ -0,0 +1,20 @@
+package com.web.timetable.timetablemanagement.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    private String id;
+
+    private String username;
+    private String password;
+    private String role;  //eg: "Admin", "Faculty", "Student"
+}
